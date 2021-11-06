ea09915d7c0e355871feec439680eac6fddc41bb
Mr Stegosaurus <mrstego123@gmail.com>
2021-11-05 20:24:28 -0400

    add some test functions

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+
+// Reverses a string
+function reverse(string) {
+  return Array.from(string).reverse().join("");
+}
+
+// Returns true for a plaindrom, false otherwise.
+function palindrome(string) {
+  let processedContent = string.toLowerCase();
+  return processedContent === reverse(processedContent);
+}
+
+// Return an array of the username and domain for a standard email
+function emailParts(email) {
+  let processedEmail = email.toLowerCase();
+  return processedEmail.split("@")
+}
